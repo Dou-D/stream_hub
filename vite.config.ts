bf8736bcf41c520538fc63cb677d8b9c3b5535c6
dou <dou20421@gmail.com>
2026-02-13 13:59:22 +0800

    feat: 重新添加pnpm-lock.yaml,并且把vitest的一些配置上传

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -8,6 +9,20 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": "/src",
+    },
+  },
+  server: {
+    proxy: {},
+  },
+  test: {
+    globals: true, // 允许直接使用 describe, it, expect 而不需要 import
+    environment: "jsdom", // 模拟浏览器环境
+    setupFiles: "./src/tests/setup.ts", // 初始化配置
+    css: false, // 通常为了速度，会禁用 CSS 处理
+    coverage: {
+      enabled: true,
+      provider: "v8",
+      include: ["src/**/*.{ts,tsx}"],
     },
   },
 });
