2806407afa7d9db443c43dc3ce529ca7b2a0cd1e
dou <dou20421@gmail.com>
2026-02-13 11:06:55 +0800

    feat: 将登录与注册的模态框交由AuthModal统一渲染，AuthModal的状态放到store中全局管理

--- src/components/HeaderMenu/index.tsx
+++ src/components/HeaderMenu/index.tsx
@@ -1,20 +1,16 @@
 import { headerMenuList } from "@/components/HeaderMenu/config";
-import { LoginModal } from "@/components/LoginCard/LoginModal";
+import { AuthModal } from "@/features/auth/components/AuthModal";
 
 export const HeaderMenu: React.FC = () => {
   return (
     <div className="hidden lg:flex items-center gap-8 justify-end">
       {headerMenuList.map((item) => (
-        <div
-          key={item.path}
-          className="flex flex-col items-center gap-1 cursor-pointer"
-        >
+        <div key={item.path} className="flex flex-col items-center gap-1 cursor-pointer">
           {item.icon}
           <span className="text-xs font-medium">{item.title}</span>
         </div>
       ))}
-      {/* <UserAvator /> 和 登录按钮 */}
-      <LoginModal />
+      <AuthModal />
     </div>
   );
 };
