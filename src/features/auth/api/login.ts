32bb3090c39b5f272af2deb92ed9faf3ec6df1e9
dou <dou20421@gmail.com>
2026-02-14 23:46:01 +0800

    refactor: 类型单独提取

--- src/features/auth/api/login.ts
+++ src/features/auth/api/login.ts
@@ -1,10 +1,10 @@
 import z from "zod";
 
-import type { LoginInputs } from "@/features/auth/components/LoginCard";
 import request from "@/utils/request";
-import { authResponseSchema } from "@/features/auth/api";
+import { authResponseSchema, loginSchema } from "@/features/auth/schema";
 
 export type LoginResponse = z.infer<typeof authResponseSchema>;
+type LoginInputs = z.infer<typeof loginSchema>;
 
 export const login = (data: LoginInputs) => {
   return request.post<LoginResponse>("/user/login", data);
--- src/features/auth/hooks/index.ts
+++ src/features/auth/hooks/index.ts
@@ -1,2 +1,3 @@
 export { useAuthGuard } from "@/features/auth/hooks/useAuthGuard";
 export { useAuth } from "@/features/auth/hooks/useAuth";
+export { useCountdown } from "@/features/auth/hooks/useCountdown";
