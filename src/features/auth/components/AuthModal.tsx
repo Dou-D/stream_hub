4613e0b7151fe082aa58ff7adfdc883b0aafe260
dou <dou20421@gmail.com>
2026-02-19 13:21:10 +0800

    feat: 将头像提取为公共组件

--- src/features/auth/components/AuthModal.tsx
+++ src/features/auth/components/AuthModal.tsx
@@ -13,6 +13,8 @@ import { X } from "lucide-react";
 import { RegisterCard } from "@/features/auth/components/RegisterCard";
 import { useUIStore } from "@/store";
 import { useAuthGuard } from "@/features/auth/hooks";
+import { UserAvatar } from "@/components/UserAvatar/UserAvatar";
+import { Link } from "react-router";
 
 export const AuthModal: React.FC = () => {
   const [isLogin, setIsLogin] = useState(true); // isLogin = true 登录模态框，false 注册模态框
@@ -29,8 +31,16 @@ export const AuthModal: React.FC = () => {
 
   if (!hasHydrated) return null; // 等待状态从存储加载完成，避免闪烁
 
+  if (isAuthenticated) {
+    return (
+      <Link className="w-10" to="/user" aria-label="进入个人中心">
+        <UserAvatar />
+      </Link>
+    );
+  }
+
   return (
-    <AlertDialog open={isAuthOpen} onOpenChange={(o) => setAuthModalState(o)}>
+    <AlertDialog open={isAuthOpen} onOpenChange={setAuthModalState}>
       <AlertDialogTrigger asChild>
         <Button type="button">登录</Button>
       </AlertDialogTrigger>
